52a7896ae0e8df821759ea1dae34391af3c230b7
Rxzh <pierre.belamri-regenpied@mines-paristech.fr>
2020-03-31 00:30:59 +0200

    fonction initialisation spline finie

--- interpolation_spline3.py
+++ interpolation_spline3.py
@@ -1,6 +1,6 @@
 import numpy as np 
 
-def calcul_spline(X,Y, F1=0 , Fn=0):
+def inti_spline(X,Y, F1=0 , Fn=0):
     n = len(X)
     h=[]
     F=[]
@@ -14,7 +14,24 @@ def calcul_spline(X,Y, F1=0 , Fn=0):
     Y = np.array(Y)
     h = np.array(h)
     F = np.array(F)
+    R = np.zeros((n,n))
+    R[0,0],R[n-1,n-1] = 1,1
+    for i in range (1,n-1):
+        R[i,i] = (h[i-1]+h[i])/3 #diagonale
+        R[i,i+1] = h[i]/6 #diagonale superieure
+        R[i,i-1] = h[i-1]/6 #diagonale inferieure
+    
+    invR = np.linalg.inv(R)
+    M = np.dot(invR , F)
+    C,Cprime = np.zeros(n-1),np.zeros(n-1)
+    for i in range(n-1):
+        C[i] = (Y[i+1]-Y[i])/h[i] - h[i]/6 * (M[i+1]-M[i])
+        Cprime[i] = Y[i] - M[i]*(h[i]**2)/6
 
     
+    return M,h,C,Cprime
+
+
+
 
 
